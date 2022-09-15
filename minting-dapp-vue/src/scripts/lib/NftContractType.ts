109b0dd7741632f841d55ad286397bd1623c9328
Vanojx1 <nicolavanoni.dev@gmail.com>
2022-09-15 15:25:54 +0200

    Update NftContractType.ts

--- minting-dapp-vue/src/scripts/lib/NftContractType.ts
+++ minting-dapp-vue/src/scripts/lib/NftContractType.ts
@@ -2,6 +2,6 @@
 // It can be updated using the following command:
 // yarn rename-contract NEW_CONTRACT_NAME
 // Please DO NOT change it manually!
-import { YourNftToken as NftContractType } from '../../../../smart-contract/typechain/index';
+import { YourNftToken as NftContractType } from '../../../../smart-contract/typechain/index'
 
-export default NftContractType;
+export default NftContractType
